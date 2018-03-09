906873a1bacbb62fc6fd6ba9ee30d9ea55a98bcc
littlestray <whatalunatik@gmail.com>
2018-03-09 12:41:53 -0500

    Neg and Pos list sort
    Re-wrote script to sort list into negative and positive changes. Next
    task is to get the deletions or additions and push those as a
    list(steam?) to Max/MSP.

--- server.js
+++ server.js
@@ -1,52 +1,80 @@
 const https = require("https");
 const cheerio = require('cheerio');
 
+
+
 console.log("Stream starting");
 
 
 let req = https.get("https://en.wikipedia.org/wiki/Special:RecentChanges?goodfaith=likelygood&hidecategorization=1&hideWikibase=1&limit=500&days=0.04166&urlversion=2",
-(res) => {
-    console.log('statusCode:', res.statusCode);
-    //console.log('headers:', res.headers);
-    let page = "";
-    res.on('data', (d) => {
-        //console.log(d.toString());
-        page += d.toString();
+    (res) => {
+        console.log('statusCode:', res.statusCode);
+        //console.log('headers:', res.headers);
+        let page = "";
+        res.on('data', (d) => {
+            //console.log(d.toString());
+            page += d.toString();
+
+        })
+
+        res.on('end', () => {
+
+            getList(page).then((x) => {
+                console.log(x);
+            }).catch((x) => {
+                console.error("getList ERR then:" + x);
+            });
+
+
+        });
 
     })
 
-    res.on('end', () => {
+function getList(page) {
+
+    return new Promise((resolve, reject) => {
+        const $ = cheerio.load(page);
+
+       
         
-        getList(page)
+
+        //console.log($('.special').text());
         
-
-    });
-
-})
-
-function getList (page) {
-
-    new Promise((resolve, reject) => {
-    const $ = cheerio.load(page);
-
-    console.log($('.special').text());
-
-    let list = $('.mw-changeslist-diff');
-    let maxList = [];
-
-    for( x in list ) {
-        
-        if(!isNaN(x)){
-        maxList[x] = {}
-        console.log(list[x] + " | " +  " | "  + x)
-        maxList[x]['title'] = list[x].attribs.title;
-        maxList[x]['diffLink'] = list[x].attribs.href;
-
-        
-        }
-    }
-
-    resolve(maxList);
+        let pos = $(".mw-plusminus-pos");
+        let neg = $(".mw-plusminus-neg");
+        let changes = {};
+        changes["pos"] = {} ;
+        changes["neg"] = {} ;
+        for (x in pos) {
+            if(x > 0){
     
+            let temp =  pos[x].parent.children.filter((x) => {return !(x.attribs == null)});
+            let tempTitle = temp[0].attribs.title;
+            let tempHref = temp[0].attribs.href;
+            
+            changes["pos"][tempTitle] = tempHref;
+
+            }
+
+
+        }
+
+        for (x in neg) {
+            if( !(neg[x].parent == undefined || neg[x].parent.children == undefined)){
+            let temp =  neg[x].parent.children.filter((x) => {return !(x.attribs == null)});
+            let tempTitle = temp[0].attribs.title;
+            let tempHref = temp[0].attribs.href;
+            
+            changes["neg"][tempTitle] = tempHref;
+            }
+        }
+
+
+
+        resolve(changes);
+
+       
+        
+
     })
 }
