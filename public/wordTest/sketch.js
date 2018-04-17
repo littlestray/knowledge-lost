053e6125a493e51677df8f1e034e056e67f043c0
littlestray <whatalunatik@gmail.com>
2018-04-17 19:03:36 -0400

    Here's our changes from the other day to the texture branch

--- public/wordTest/sketch.js
+++ public/wordTest/sketch.js
@@ -11,7 +11,7 @@ function preload() {
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  wordSystem = new WordSystem();
+  
   background(0);
 
   var socket = io.connect('http://localhost:2222');
@@ -25,19 +25,17 @@ function setup() {
 
 function socketUpdate(obj) {
   var Thelist = Object.keys(obj);
-  console.log(wordSystem.words.length + " fr: " + frameRate());
+  console.log("fr: " + frameRate());
   for (var i = 0; i < obj[Thelist[0]].length; i++) {
     if (obj[Thelist[0]][i]) {
       if (frameRate() > 15) {
-        wordSystem.addWord(obj[Thelist[0]][i], i);
+        displayWord(obj[Thelist[0]][i]);
+        
       }
     }
   }
-
-
-
-
 }
+
 function mousePressed() {
   // The fullscreen was making debugging difficult.
   // var fs = fullscreen();
@@ -45,113 +43,33 @@ function mousePressed() {
 
 }
 
-let Word = function (theWord, preLifeFrames) {
+function displayWord(theWord) {
 
   this.size = floor(random(40) + 42);
   this.word = theWord;
   this.x = random(windowWidth);
   this.y = random(windowHeight);
-  this.life = 255.;
-  this.tick = random(1.) + 0.5 * 1.25;
-  //Chris
-  this.isBorn = false;
-  this.preLife = preLifeFrames;
 
-
-
-};
-Word.prototype.run = function (fr) {
-  this.update(fr);
-  this.display();
-};
-Word.prototype.display = function () {
   textSize(this.size);
   textAlign(CENTER);
   textFont(myFont);
-  fill(this.life, this.life, this.life);
+  fill(255,255,255,100);
   text(this.word, this.x, this.y);
 
-
-
 };
 
-
-
-Word.prototype.birth = function () {
-  this.isBorn = true;
-
-
-}
-
-Word.prototype.talk = function () {
-
-}
-
-Word.prototype.update = function (frameRate) {
-  this.life = this.life - (this.tick * 60/frameRate) ;
-  //this.life = this.life - (this.tick);
-};
-
-Word.prototype.isDead = function () {
-  if (this.life < 0) {
-    return true;
-    //console.log(this.word);
-  } else {
-    return false;
-  }
-};
-
-let WordSystem = function () {
-  this.words = [];
-};
-
-WordSystem.prototype.addWord = function (newWord, index) {
-  let tempWord = new Word(newWord, index * 8);
-  this.words.push(tempWord);
-
-
-
-  // setTimeout(function () {
-  //   //console.log(newWord)
-  //   this.words.push(new Word(newWord));
-  //   console.log(index * 200 + 1)
-  // }, index * 200 + 1);
-
-};
-
-WordSystem.prototype.run = function () {
-  let fr = frameRate();
-  for (var i = this.words.length - 1; i >= 0; i--) {
-    let w = this.words[i];
-
-    if (w.isBorn) {
-      w.run(fr);
-      if (w.isDead()) {
-        this.words[i] = null;
-        this.words.splice(i, 1);
-      } 
-    } else {
-
-      
-      if (w.preLife == 0) {
-        w.isBorn = true;
-        // console.log(w.theWord)
-        if (!responsiveVoice.isPlaying()) {
-          responsiveVoice.speak(w.word);
-        }
-      } else {
-        w.preLife--
-      }
-
-    }
-
-  }
-};
+let counter = 0;
 
 function draw() {
-  clear();
-  background(0);
-  wordSystem.run();
-  // console.log(wordSystem.words.length);
+  counter += 0.01;
+  //background(0, 0, 0, counter % 255);
+  fill(0, 12);
+  rect(0,0,windowWidth,windowHeight);
+  // loadPixels();
+  // for (var i = 0; i < pixels.length; i++) {
+  //   for(j)
+  // }
+  // updatePixels();
+  
 
 }
